25fca3fc1fc5ebc64f0e331cb74f46e9ffa14f08
Aimar Rodriguez <aimar.rodriguez.s@gmail.com>
2014-07-17 13:00:28 +0200

    Project scaffolded

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('forumjsApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
